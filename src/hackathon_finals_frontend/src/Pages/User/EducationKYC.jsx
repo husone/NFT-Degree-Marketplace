fab6c3d2ad61ae6b3417d29747b9611d13661b86
daovo739 <daovo739@gmail.com>
2022-08-17 16:28:35 +0700

    done function to pass education kyc to server

--- src/hackathon_finals_frontend/src/Pages/User/EducationKYC.jsx
+++ src/hackathon_finals_frontend/src/Pages/User/EducationKYC.jsx
@@ -1,10 +1,14 @@
 import { useEffect, useState } from 'react'
 import { useConnect } from '@connect2ic/react'
 import { PlugWallet } from '@connect2ic/core/providers/plug-wallet'
+import axios from 'axios'
 
 function EducationKYC() {
   const { principal, connect, isConnected } = useConnect()
   const [education, setEducation] = useState({})
+  const [imgUri, setImgUri] = useState('')
+  const [file, setFile] = useState()
+  const [fileName, setFileName] = useState('')
 
   useEffect(() => {
     const connectWallet = async () => {
@@ -18,19 +22,58 @@ function EducationKYC() {
   const handleChange = event => {
     const name = event.target.name
     const value = event.target.value
-    setEducation(values => ({ ...values, [name]: value }))
+    setEducation(values => ({
+      ...values,
+      [name]: value,
+    }))
   }
 
-  const handleSubmit = e => {
+  const handleSubmit = async e => {
     e.preventDefault()
-    console.log(education)
+    const formData = new FormData()
+    for (let key in education) {
+      formData.append(key, education[key])
+    }
+
+    const res = await axios.post(
+      'http://localhost:5000/api/v1/education-kyc',
+      formData
+    )
+
+    // Doing something to notification to user
+    if (res.status === 201) {
+      console.log('success')
+    } else {
+      console.log('error')
+    }
+  }
+
+  const getFile = e => {
+    let file = e.target.files[0]
+    setFile(file)
+    setFileName(file.name)
+    if (file) {
+      const reader = new FileReader()
+      reader.onload = () => {
+        let result = reader.result
+        setImgUri(result)
+        setEducation(values => ({
+          ...values,
+          image: file,
+          principal,
+        }))
+      }
+      reader.readAsDataURL(file)
+    }
   }
 
   return (
     <div>
       <h1>Education KYC page</h1>
-      <form onSubmit={handleSubmit}>
+      {/* FORM */}
+      <form onSubmit={handleSubmit} encType="multipart/form-data">
         <label htmlFor="name">Your center education name</label>
+        {/* INPUT */}
         <input
           type="text"
           name="name"
@@ -39,21 +82,12 @@ function EducationKYC() {
           onChange={handleChange}
           required
         />
-        <label htmlFor="tax">Tax code</label>
+        <label htmlFor="legalRepresentative">Legal representative</label>
         <input
           type="text"
-          name="tax"
-          id="tax"
-          value={education.tax || ''}
-          onChange={handleChange}
-          required
-        />
-        <label htmlFor="rep">Legal representative</label>
-        <input
-          type="text"
-          name="rep"
-          id="rep"
-          value={education.rep || ''}
+          name="legalRepresentative"
+          id="legalRepresentative"
+          value={education.legalRepresentative || ''}
           onChange={handleChange}
           required
         />
@@ -66,6 +100,25 @@ function EducationKYC() {
           onChange={handleChange}
           required
         />
+
+        {/* UPLOAD FILE, IMG PREVIEW */}
+        <div className="wrap-upload input-group mb-3 d-flex justify-content-center">
+          {imgUri && <img className="previewImg" src={imgUri} alt="preview" />}
+          <input
+            type="file"
+            name="file"
+            id="fileUpload"
+            accept=".jpeg,.jpg,.png,.gif,image/*"
+            onChange={e => getFile(e)}
+            required
+          />
+          <label htmlFor="fileUpload">
+            <div className="upload_label">
+              {/* <img src={UploadGif} alt="upload gif" /> */}
+              <h3>Click to upload Item</h3>
+            </div>
+          </label>
+        </div>
         <input type="submit" value="Submit" />
       </form>
     </div>
