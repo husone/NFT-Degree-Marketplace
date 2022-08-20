48d06691abe03be1eecff4a6112b8e66705deb2f
vuongbachdoan <vuongbachdoan@gmail.com>
2022-08-20 13:29:48 +0700

    UI: add home page UI

--- src/hackathon_finals_frontend/src/Pages/Home/Home.jsx
+++ src/hackathon_finals_frontend/src/Pages/Home/Home.jsx
@@ -1,5 +1,9 @@
 import React, { useState } from 'react'
-import { final_be } from '../../../../declarations/final_be'
+import { final_be } from '../../../../declarations/final_be';
+import { Input, Tabs } from "antd";
+import Item from "./Item"
+const { Search } = Input;
+const { TabPane } = Tabs;
 
 function Home() {
   const [size, setSize] = useState('large')
@@ -9,10 +13,38 @@ function Home() {
     console.log(res)
   }
 
+  const onSearch = () => {
+  }
+
+  const onChange = (key) => {
+    console.log(key);
+  };
+
   hello()
   return (
     <div>
-      <h1> Home page</h1>
+      <Search
+        placeholder="input search text"
+        allowClear
+        enterButton="Search"
+        size="large"
+        onSearch={onSearch}
+        className="my-4"
+      />
+
+      <h1>Explore Collections</h1>
+
+      <Tabs defaultActiveKey="1" onChange={onChange}>
+        <TabPane tab="Search" key="1" className="my-5">
+          <Item/>
+        </TabPane>
+        <TabPane tab="Trending" key="2">
+          Content of Tab Pane 2
+        </TabPane>
+        <TabPane tab="Top" key="3">
+          Content of Tab Pane 3
+        </TabPane>
+      </Tabs>
     </div>
   )
 }
