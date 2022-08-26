0609dc0a3f6104b5c67c2b52a4b8f498f567633c
vuongbachdoan <vuongbachdoan@gmail.com>
2022-08-27 00:23:24 +0700

    UI: add home

--- src/hackathon_finals_frontend/src/Pages/Home/Home.jsx
+++ src/hackathon_finals_frontend/src/Pages/Home/Home.jsx
@@ -7,7 +7,83 @@ import { Link } from 'react-router-dom'
 import MyNFTItem from '../User/MyNFTItem'
 import IMAGES from '../../Assets/IMAGE'
 import Carousel from 'react-bootstrap/Carousel'
+import CoinLogo from '../../Assets/Images/DBZcoin.png'
 
+const topCenter = [
+  {
+    name: "FPT University",
+    uri: "https://dnuni.fpt.edu.vn/wp-content/uploads/2021/11/FPTUDN-3-1598x900.png"
+  },
+  {
+    name: "FPT University",
+    uri: "https://dnuni.fpt.edu.vn/wp-content/uploads/2021/11/FPTUDN-3-1598x900.png"
+  },
+  {
+    name: "FPT University",
+    uri: "https://dnuni.fpt.edu.vn/wp-content/uploads/2021/11/FPTUDN-3-1598x900.png"
+  },
+  {
+    name: "FPT University",
+    uri: "https://dnuni.fpt.edu.vn/wp-content/uploads/2021/11/FPTUDN-3-1598x900.png"
+  },
+  {
+    name: "FPT University",
+    uri: "https://dnuni.fpt.edu.vn/wp-content/uploads/2021/11/FPTUDN-3-1598x900.png"
+  },
+  {
+    name: "FPT University",
+    uri: "https://dnuni.fpt.edu.vn/wp-content/uploads/2021/11/FPTUDN-3-1598x900.png"
+  },
+  {
+    name: "FPT University",
+    uri: "https://dnuni.fpt.edu.vn/wp-content/uploads/2021/11/FPTUDN-3-1598x900.png"
+  },
+  {
+    name: "FPT University",
+    uri: "https://dnuni.fpt.edu.vn/wp-content/uploads/2021/11/FPTUDN-3-1598x900.png"
+  },
+  {
+    name: "FPT University",
+    uri: "https://dnuni.fpt.edu.vn/wp-content/uploads/2021/11/FPTUDN-3-1598x900.png"
+  },
+  {
+    name: "FPT University",
+    uri: "https://dnuni.fpt.edu.vn/wp-content/uploads/2021/11/FPTUDN-3-1598x900.png"
+  }
+]
+
+const topCertificate = [
+  {
+    owner: "H.T.Ha",
+    uri: "https://lambang-toanquoc.com/wp-content/uploads/2020/10/lam-bang-dai-hoc-cntt1-1536x1093.jpg",
+    ownerUri: "https://ps.w.org/user-avatar-reloaded/assets/icon-256x256.png?rev=2540745"
+  },
+  {
+    owner: "H.T.Ha",
+    uri: "https://lambang-toanquoc.com/wp-content/uploads/2020/10/lam-bang-dai-hoc-cntt1-1536x1093.jpg",
+    ownerUri: "https://ps.w.org/user-avatar-reloaded/assets/icon-256x256.png?rev=2540745"
+  },
+  {
+    owner: "H.T.Ha",
+    uri: "https://lambang-toanquoc.com/wp-content/uploads/2020/10/lam-bang-dai-hoc-cntt1-1536x1093.jpg",
+    ownerUri: "https://ps.w.org/user-avatar-reloaded/assets/icon-256x256.png?rev=2540745"
+  },
+  {
+    owner: "H.T.Ha",
+    uri: "https://lambang-toanquoc.com/wp-content/uploads/2020/10/lam-bang-dai-hoc-cntt1-1536x1093.jpg",
+    ownerUri: "https://ps.w.org/user-avatar-reloaded/assets/icon-256x256.png?rev=2540745"
+  },
+  {
+    owner: "H.T.Ha",
+    uri: "https://lambang-toanquoc.com/wp-content/uploads/2020/10/lam-bang-dai-hoc-cntt1-1536x1093.jpg",
+    ownerUri: "https://ps.w.org/user-avatar-reloaded/assets/icon-256x256.png?rev=2540745"
+  },
+  {
+    owner: "H.T.Ha",
+    uri: "https://lambang-toanquoc.com/wp-content/uploads/2020/10/lam-bang-dai-hoc-cntt1-1536x1093.jpg",
+    ownerUri: "https://ps.w.org/user-avatar-reloaded/assets/icon-256x256.png?rev=2540745"
+  }
+]
 const { Search } = Input
 const { TabPane } = Tabs
 const { header_1, header_2, header_3, icon_1, icon_2, icon_3, icon_4 } = IMAGES
@@ -25,7 +101,7 @@ function Home() {
     setListNFT(res)
   }
 
-  const onSearch = () => {}
+  const onSearch = () => { }
 
   const onChange = key => {
     console.log(key)
@@ -90,31 +166,36 @@ function Home() {
         </button>
       </div>
 
-      <section>
-        <div className="container">
-          <div className="row">
-            <div className="col-md-12">
-              <div className="sc-heading style-2">
-                <div className="content-left">
-                  <div className="inner">
-                    <h3 className="">Top Centers</h3>
-                    <p className="desc">Most popular education centers </p>
-                  </div>
-                </div>
+      <section className="tf-best-seller">
+        <div className="best-seller-inner">
+          <div className="container">
+            <div className="row my-3">
+              <div className="col">
+                <h3 className="text-light">Top Centers</h3>
+                <p className="desc text-muted">Most popular education centers </p>
+              </div>
+              <div className="col d-flex justify-content-end">
+                <button className="sc-button style letter style-2"><span>Explore More</span> </button>
               </div>
             </div>
-            <div className="col-xl-4 col-lg-6 col-md-6">
-              <div className="sc-author style-2">
-                <div className="card-avatar">
-                  <img src={header_1} alt="" className="" />
-                </div>
-                <div className="infor">
-                  <h5>
-                    <p href="author.html">Frank F. Chan</p>
-                  </h5>
-                  <div className="details">523.7 ETH</div>
-                </div>
-              </div>
+            <div className="center_wrapper">
+              {
+                topCenter.map((center, index) => {
+                  return (
+                    <div className="sc-author col">
+                      <div className="p-1">
+                        <div className="card-avatar mb-4">
+                          <img src={center.uri} alt="center uri" />
+                        </div>
+                        <div className="infor">
+                          <h6> <a className='text-light' href="author.html">{center.name}</a> </h6>
+                        </div>
+                        <a href="#" className="top_center_btn"><span>Follow</span></a>
+                      </div>
+                    </div>
+                  );
+                })
+              }
             </div>
           </div>
         </div>
@@ -127,25 +208,34 @@ function Home() {
               <div className="sc-heading style-2">
                 <div className="content-left">
                   <div className="inner">
-                    <h3 className="">Top Centers</h3>
-                    <p className="desc">Most popular education centers </p>
+                    <h3 className="text-light">Top Certificates</h3>
+                    <p className="desc text-muted">Most favorite certificates</p>
+                    <div className='certificates_wrapper'>
+                      {
+                        topCertificate.map((certificate, index) => {
+                          return (
+                            <div>
+                              <div className="cer_img">
+                                <img src={certificate.uri} alt="certificate uri" />
+                              </div>
+                              <h3 className="description mt-3 mx-3 text-light">"This is description for certificate"</h3>
+                              <div className="d-flex cer_content mx-3 my-3">
+                                <img src={certificate.ownerUri} alt="owner uri" />
+                                <div className='ms-3'>
+                                  <h3 className='text-light m-0'>{certificate.owner}</h3>
+                                  <p className='text-muted'>Owner</p>
+                                </div>
+                              </div>
+                              <div className='px-3 py-3 text-center' style={{backgroundColor: "#14161b"}}><b>Price - </b>8 DBZ <img className='coin_logo' src={CoinLogo} alt="coin logo" /></div>
+                            </div>
+                          );
+                        })
+                      }
+                    </div>
                   </div>
                 </div>
               </div>
             </div>
-            {/* <div className="col-xl-4 col-lg-6 col-md-6">
-              <div className="sc-author style-2">
-                <div className="card-avatar">
-                  <img src={image1} alt="" className="" />
-                </div>
-                <div className="infor">
-                  <h5>
-                    <p href="author.html">Frank F. Chan</p>
-                  </h5>
-                  <div className="details">523.7 ETH</div>
-                </div>
-              </div>
-            </div> */}
             {listNFT.map(nft => {
               const id = Number(nft?.id)
               return (
@@ -167,7 +257,7 @@ function Home() {
               <div className="sc-heading style-2 create-and-sell">
                 <div className="content-left">
                   <div className="inner">
-                    <h3 className="">Create and sell your NFTs</h3>
+                    <h3 className="text-light">Create and sell your NFTs</h3>
                     <p className="desc">
                       Most popular gaming digital nft market place{' '}
                     </p>
@@ -182,7 +272,7 @@ function Home() {
                 </div>
                 <div className="content st-current">
                   <h5 className="">
-                    <a href="" className="">
+                    <a href="" className="gradient-heading">
                       Set Up Your Wallet
                     </a>
                   </h5>
@@ -202,7 +292,7 @@ function Home() {
                 </div>
                 <div className="content">
                   <h5 className="">
-                    <a href="" className="">
+                    <a href="" className="gradient-heading">
                       Create Your Collection
                     </a>
                   </h5>
@@ -222,7 +312,7 @@ function Home() {
                 </div>
                 <div className="content">
                   <h5>
-                    <a href="author.html">Add Your NFTs</a>
+                    <a href="author.html" className='gradient-heading'>Add Your NFTs</a>
                   </h5>
                   <p className="">
                     Sed ut perspiciatis unde omnste natus error sit voluptatem{' '}
@@ -254,16 +344,6 @@ function Home() {
           </div>
         </div>
       </section>
-      {/* <Tabs defaultActiveKey="1" onChange={onChange}>
-        <TabPane tab="Search" key="1" className="my-5">
-        </TabPane>
-        <TabPane tab="Trending" key="2">
-          Content of Tab Pane 2
-        </TabPane>
-        <TabPane tab="Top" key="3">
-          Content of Tab Pane 3
-        </TabPane>
-      </Tabs> */}
     </div>
   )
 }
